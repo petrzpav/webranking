8f591be3f16bc4a80234403389f4d67f8fbe0ff9
Tomáš Martínek <martit12@fit.cvut.cz>
2017-05-14 21:14:36 +0200

    starting frontend

--- src/webranking/index.js
+++ src/webranking/index.js
@@ -1,18 +1,38 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 app.get('/', function (req, res) {
-  const home = require('fs')
+  const fs = require('fs')
   try {
-    var data = home.readFileSync('home.html', 'utf8')
+    const data = fs.readFileSync('home.html', 'utf8')
     res.send(data)
   } catch (e) {
     console.log('Error:', e.stack)
   }
 })
 
+app.post('/query', function (req, res) {
+  const fs = require('fs')
+  try {
+    const queryWord = req.body.queryWord
+    const data = fs.readFileSync('response.html', 'utf8')
+
+    const answer = data.toString().replace('#queryWord#', queryWord)
+
+    res.send(answer)
+  } catch (e) {
+    console.log('Error:', e.stack)
+  }
+})
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Webranking 1.0.0 app listening on port 3000!')
 })
