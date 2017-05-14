efef9990f834f4db8bc4c18544384eaeab95f7f2
Tomáš Martínek <martit12@fit.cvut.cz>
2017-05-14 19:01:34 +0200

    possible frontend solution

--- src/webranking/index.js
+++ src/webranking/index.js
@@ -1,6 +1,18 @@
 'use strict'
 
+const express = require('express')
+const app = express()
 
-exports.xx = () => {
-  console.log('xx')
-}
+app.get('/', function (req, res) {
+  const home = require('fs')
+  try {
+    var data = home.readFileSync('home.html', 'utf8')
+    res.send(data)
+  } catch (e) {
+    console.log('Error:', e.stack)
+  }
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
